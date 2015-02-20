d6a6c0043691a0a00f76fce2820fa3416d18791f
Neil Hastings <neil.hastings@gmail.com>
2015-02-20 09:21:16 -0500

    Initial Project module:  https://github.com/appirio-tech/np-front-end-reference/issues/3

--- src/client/test-helpers/mock-data.js
+++ src/client/test-helpers/mock-data.js
@@ -34,44 +34,44 @@ var mockData = (function () {
       {firstName: 'Haley', lastName: 'Guthrie', age: 35, location: 'Wyoming'}
     ];
   }
-  
+
   function getMockProjects() {
     return [
       {
-        "id": "30000007",
-        "projectName": "Client 30000001 Billing Account 3 Web API Project 1",
-        "projectStatusId": 3,
-        "projectStatusName": "Cancelled",
-        "projectCreatedDate": "12/25/2014 07:12",
-        "projectCreatedBy": 132458,
-        "projectLastUpdatedDate": "12/27/2014 01:18",
-        "billings": [
+        'id': '30000007',
+        'projectName': 'Client 30000001 Billing Account 3 Web API Project 1',
+        'projectStatusId': 3,
+        'projectStatusName': 'Cancelled',
+        'projectCreatedDate': '12/25/2014 07:12',
+        'projectCreatedBy': 132458,
+        'projectLastUpdatedDate': '12/27/2014 01:18',
+        'billings': [
           {
-            "id": "30000003",
-            "name": "Client 30000001 Secret Billing Account 3"
+            'id': '30000003',
+            'name': 'Client 30000001 Secret Billing Account 3'
           }
         ]
       },
       {
-        "id": "30000001",
-        "projectName": "Client 30000001 Billing Account 1 Web Application Project 1",
-        "projectStatusId": 2,
-        "projectStatusName": "Inactive",
-        "projectCreatedDate": "12/31/2014 01:12",
-        "projectCreatedBy": 132458,
-        "projectLastUpdatedDate": "12/31/2014 07:18",
-        "billings": [
+        'id': '30000001',
+        'projectName': 'Client 30000001 Billing Account 1 Web Application Project 1',
+        'projectStatusId': 2,
+        'projectStatusName': 'Inactive',
+        'projectCreatedDate': '12/31/2014 01:12',
+        'projectCreatedBy': 132458,
+        'projectLastUpdatedDate': '12/31/2014 07:18',
+        'billings': [
           {
-            "id": "30000001",
-            "name": "Client 30000001 Major Billing Account 1"
+            'id': '30000001',
+            'name': 'Client 30000001 Major Billing Account 1'
           },
           {
-            "id": "30000002",
-            "name": "Client 30000001 Minor Billing Account 2"
+            'id': '30000002',
+            'name': 'Client 30000001 Minor Billing Account 2'
           },
           {
-            "id": "30000003",
-            "name": "Client 30000001 Secret Billing Account 3"
+            'id': '30000003',
+            'name': 'Client 30000001 Secret Billing Account 3'
           }
         ]
       }
