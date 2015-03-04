2a7a7e269e47609388cb6af55207de1f631e63fd
Neil Hastings <neil.hastings@gmail.com>
2015-03-04 12:25:14 -0500

    Initial env configuration usage

---
+++ config/index.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var fs = require('fs');
+var env = require('node-env-file');
+
+if (fs.existsSync(path.join(__dirname, '../.env'))) {
+  env(path.join(__dirname, '../.env'));
+}
+
+module.exports = {
+  getVal: getVal
+};
+
+function getVal(name, defaultVal) {
+  if (process.env.hasOwnProperty(name)) {
+    return process.env[name].trim();
+  }
+  else if (defaultVal) {
+    return defaultVal;
+  }
+  else {
+    throw new Error('Env setting: ' + name + ' is not configured!');
+  }
+}
