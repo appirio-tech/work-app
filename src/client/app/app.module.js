d0afc4bbb6247cbe921517bf9ee30bc26738f544
Alex Tran <vietnogi@gmail.com>
2015-03-09 11:12:46 -0700

    remove misc stuff

---
+++ src/client/app/app.module.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular.module('app', [
+    'app.core',
+    'app.widgets',
+    'app.admin',
+    'app.layout',
+    'app.project.manage',
+    'app.project.create',
+    'app.project.core'
+  ]);
+
+})();
