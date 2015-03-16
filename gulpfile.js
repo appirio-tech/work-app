98822ef9106714eb1aa25966499f0c5375935769
Neil Hastings <neil.hastings@gmail.com>
2015-03-16 13:35:01 -0400

    Run tests when we build

--- gulpfile.js
+++ gulpfile.js
@@ -193,7 +193,7 @@ gulp.task('build-specs', ['inject'], function (done) {
  * This is separate so we can run tests on
  * optimize before handling image or fonts
  */
-gulp.task('build', ['optimize', 'images', 'fonts'], function () {
+gulp.task('build', ['optimize', 'images', 'fonts', 'test'], function () {
   log('Building everything');
 
   var msg = {
@@ -386,7 +386,7 @@ gulp.task('bump', function () {
 /**
  * Push build folder to s3
  */
-gulp.task('deploy', ['build', 'test'], function() {
+gulp.task('deploy', ['build'], function() {
   var awsConfig = {
     bucket: config.aws.bucket,
     key: config.aws.key,
