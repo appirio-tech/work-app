cbb19a6dc0c09ca83f01d29ee52f793639d43d97
r-jain <rohitjain.pr@gmail.com>
2015-03-27 00:26:13 +0530

    resolved build error

--- src/client/app/core/core.module.js
+++ src/client/app/core/core.module.js
@@ -5,7 +5,7 @@
     .module('app.core', [
       'ngAnimate', 'ngSanitize',
       'blocks.exception', 'blocks.logger', 'blocks.router',
-      'ui.router', 'ngplus',
-      'app.resource'
+      'ui.router', 'ngplus'
+      //'app.resource'
     ]);
 })();
