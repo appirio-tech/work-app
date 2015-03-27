5e9df0093b65139417e552b92505d692fa831542
Tom Ladendorf <tom@tomladendorf.com>
2015-03-27 14:15:18 -0500

    Added estimates and launch with templates and logic

---
+++ src/client/app/submit-work/launch/scripts/estimate.controller.js
@@ -0,0 +1,31 @@
+/*global form:true */
+(function () {
+  'use strict';
+
+  angular
+    .module('app.submit-work')
+    .controller('SubmitEstimateController', SubmitEstimateController);
+
+  SubmitEstimateController.$inject = ['logger', '$state', 'SubmitWorkService'];
+  /* @ngInject */
+  function SubmitEstimateController(logger, $state, SubmitWorkService) {
+    var vm = this;
+    vm.title = 'Estimate';
+    vm.work = {};
+    vm.estimate = '$8,000 - $10,000';
+    vm.termsAccepted = false;
+    vm.next = next;
+
+    activate();
+
+    function activate() {
+      logger.info('Activated Estimate View');
+      vm.work = SubmitWorkService.getCurrent();
+    }
+
+    function next() {
+      if (vm.termsAccepted) SubmitWorkService.next('launch-success')();
+    }
+
+  }
+})();
