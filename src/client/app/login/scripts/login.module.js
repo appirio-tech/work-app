220fa8d96690cde93ed9907e03b3689be72e6980
Neil Hastings <neil.hastings@gmail.com>
2015-04-01 11:15:33 -0400

    DEM-159, DEM-160 Auth fixes

--- src/client/app/login/scripts/login.module.js
+++ src/client/app/login/scripts/login.module.js
@@ -3,15 +3,41 @@
 
   angular.module('app.login', [
     'angular-storage',
-    'angular-jwt'
+    'angular-jwt',
+    'app.constants'
   ]).config(JwtConfig);
 
   JwtConfig.$inject = ['$httpProvider', 'jwtInterceptorProvider'];
 
   function JwtConfig($httpProvider, jwtInterceptorProvider) {
-    function jwtInterceptor() {
-      //@TODO handle expired tokens
-      return localStorage.getItem('userJWTToken');
+    jwtInterceptor.$inject = ['jwtHelper', '$http', 'apiUrl'];
+
+    function jwtInterceptor(jwtHelper, $http, apiUrl) {
+      var idToken = localStorage.getItem('userJWTToken');
+
+      if (idToken) {
+        if (jwtHelper.isTokenExpired(idToken)) {
+          // This is a promise of a JWT id_token
+          // @TODO put this in a service
+          return $http({
+            url: apiUrl + 'authorizations/1',
+            // This makes it so that this request doesn't send the JWT
+            skipAuthorization: true,
+            method: 'GET',
+            headers: {
+              'Authorization': 'Bearer ' + idToken
+            }
+          }).then(function(response) {
+            var id_token = response.data.id_token;
+            localStorage.setItem('userJWTToken', id_token);
+            return id_token;
+          });
+        } else {
+          return idToken;
+        }
+      } else {
+        return '';
+      }
     }
 
     jwtInterceptorProvider.tokenGetter = jwtInterceptor;
@@ -19,3 +45,5 @@
     $httpProvider.interceptors.push('jwtInterceptor');
   }
 })();
+
+
