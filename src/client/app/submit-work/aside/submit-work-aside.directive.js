eaa7d16a23058704d174a9ecda7270f3b33b124d
Alex Tran <vietnogi@gmail.com>
2015-04-02 16:22:49 -0700

    [DEM-147]

--- src/client/app/submit-work/aside/submit-work-aside.directive.js
+++ src/client/app/submit-work/aside/submit-work-aside.directive.js
@@ -1,17 +1,29 @@
 (function () {
   'use strict';
 
-  var directive = function ($window) {
+  var directive = function ($window, $rootScope) {
     var link = function (scope, element, attrs) {
       var setFixed = function () {
-        element.removeClass('fixed');
-
-        if (this.pageYOffset >= 100) {
+        if ($window.scrollY >= 100) {
           element.addClass('fixed');
+        }
+        else {
+          element.removeClass('fixed');
         }
       };
 
       angular.element($window).bind('scroll', setFixed);
+
+      setFixed();
+
+      // needs refactoring when more time
+      $rootScope.$on('scroll-state', function (rootScope, state) {
+        element.find('.state-active').removeClass('state-active');
+
+        element.find('[state="' + state + '"]').addClass('state-active');
+      });
+
+      element.find('[state="' + $rootScope.scrollState + '"]').addClass('state-active');
     };
 
     return {
@@ -21,5 +33,7 @@
     };
   };
 
-  angular.module('app.submit-work').directive('ngSubmitWorkAside', ['$window', directive]);
+  directive.$inject = ['$window', '$rootScope'];
+
+  angular.module('app.submit-work').directive('ngSubmitWorkAside', directive);
 })();
