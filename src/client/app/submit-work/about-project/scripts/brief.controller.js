b102d591b454770a98b1523a0acfe045bca58645
Alex Tran <vietnogi@gmail.com>
2015-04-06 10:58:48 -0700

    [DEM-208] We are missing the YES , No page

--- src/client/app/submit-work/about-project/scripts/brief.controller.js
+++ src/client/app/submit-work/about-project/scripts/brief.controller.js
@@ -9,11 +9,32 @@
   SubmitBriefController.$inject = ['logger', '$state', 'SubmitWorkService'];
   /* @ngInject */
   function SubmitBriefController(logger, $state, SubmitWorkService) {
-    var vm = this;
-    vm.title = 'Brief';
-    vm.project = {};
+    var vm           = this;
+    vm.title         = 'Brief';
+    vm.project       = {};
     vm.briefFilename = '';
-    vm.nextState = 'competitors';
+    vm.nextState     = 'competitors';
+    vm.showYesNo     = true;
+    vm.showBrief     = false;
+    vm.showElevator  = false;
+
+    vm.toggleYes = function () {
+      vm.showYesNo    = false;
+      vm.showBrief    = true;
+      vm.showElevator = false;
+    }
+
+    vm.toggleNo = function () {
+      vm.showYesNo    = false;
+      vm.showBrief    = false;
+      vm.showElevator = true;
+    }
+
+    vm.toggleCancel = function () {
+      vm.showYesNo    = true;
+      vm.showBrief    = false;
+      vm.showElevator = false;
+    }
 
     activate();
 
