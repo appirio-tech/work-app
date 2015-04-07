63bf38f06f800c4ad412aebc81c56120bb9c5d47
Tom Ladendorf <tom@tomladendorf.com>
2015-04-07 15:31:44 -0700

    Fixed 'next' button for project type

--- src/client/app/submit-work/about-project/scripts/type.controller.js
+++ src/client/app/submit-work/about-project/scripts/type.controller.js
@@ -6,9 +6,9 @@
     .module('app.submit-work')
     .controller('SubmitTypeController', SubmitTypeController);
 
-  SubmitTypeController.$inject = ['logger', 'SubmitWorkService', '$state'];
+  SubmitTypeController.$inject = ['logger', 'SubmitWorkService', '$state', '$scope'];
   /* @ngInject */
-  function SubmitTypeController(logger, SubmitWorkService, $state) {
+  function SubmitTypeController(logger, SubmitWorkService, $state, $scope) {
     var vm = this;
     vm.title = 'Type';
     vm.work = {};
