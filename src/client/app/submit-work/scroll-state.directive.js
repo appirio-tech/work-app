55245619737923e069837d74359ff26fc3daba19
Alex Tran <vietnogi@gmail.com>
2015-04-09 17:30:42 -0700

    went back to old scroll

--- src/client/app/submit-work/scroll-state.directive.js
+++ src/client/app/submit-work/scroll-state.directive.js
@@ -5,116 +5,60 @@
     var link = function (scope, element, attrs) {
       var stateElements      = element.find('.state');
       var previousScrollTop  = $document.scrollTop();
-      var activeStateElement = stateElements.eq(0);
-      var nextStateElement   = stateElements.eq(1);
       var isAutoScrolling    = false;
-      var prevStateElement;
-      var initialAutoScroll = false;
+      var isManualScrolling  = false;
 
       function setActiveStateElement () {
+        var activeStateElement = stateElements.eq(0);
+
         stateElements.each(function (i, state) {
           var startingLine = element.find('#starting-line-' + stateElements.eq(i).attr('state'));
+          var hitThreshold = startingLine.offset().top <= $document.scrollTop();
 
-          if (startingLine.offset().top <= $document.scrollTop()) {
+          if (hitThreshold) {
             activeStateElement = stateElements.eq(i);
-
-            if (i == 0) {
-              prevStateElement = false;
-            }
-            else {
-              prevStateElement = stateElements.eq(i - 1);
-            }
-
-            if (i == stateElements.length - 1) {
-              false;
-            }
-            else {
-              nextStateElement = stateElements.eq(i + 1);
-            }
           }
         });
+
+        stateElements.removeClass('state-active');
+
+        activeStateElement.addClass('state-active');
+
+        return activeStateElement;
       }
 
       var manualScrolling = function (e) {
-        console.log(e);
-        setActiveStateElement();
+        if (!isAutoScrolling) {
+          var activeState = setActiveStateElement().attr('state');
 
-        if (isAutoScrolling) {
-          console.log('auto scrolling...');
-          return true;
-        }
-
-        console.log('manual scrolling...');
-
-        var nextState, hitThreshold;
-        var scrollingDown = previousScrollTop < $document.scrollTop();
-
-        previousScrollTop = $document.scrollTop();
-
-        nextState = activeStateElement.attr('state');
-
-        if (scrollingDown && nextStateElement) {
-          hitThreshold = activeStateElement.offset().top < $document.scrollTop();
-
-          if (hitThreshold) {
-            nextState = nextStateElement.attr('state');
+          if (scope.activeState != activeState) {
+            isManualScrolling = true;
+            scope.activeState = activeState;
+            scope.$apply();
           }
-        }
-
-        if (!scrollingDown && prevStateElement) {
-          var previousFinishLine = element.find('#finish-line-' + prevStateElement.attr('state'));
-
-          hitThreshold = previousFinishLine.offset().top > $document.scrollTop();
-
-          if (hitThreshold) {
-            nextState = prevStateElement.attr('state');
-          }
-        }
-
-        if (!e) {
-          initialAutoScroll = true;
-        }
-
-        if (scope.activeState != nextState) {
-          scope.activeState = nextState;
-          scope.$apply();
         }
       };
 
       var autoScrolling = function (state) {
-        if (state) {
+        if (isManualScrolling) {
+          isManualScrolling = false;
+        }
+        else if (state) {
           var stateElement = element.find('#starting-line-' + state);
+          isAutoScrolling = true;
 
           var finishedScrolling = function () {
             isAutoScrolling = false;
-
-            console.log('turn off auto scrolling.');
-
-            stateElements.removeClass('state-active');
-
-            element.find('[state="' + state + '"]').addClass('state-active');
-
             setActiveStateElement();
           };
 
-          isAutoScrolling = true;
-
-          if (initialAutoScroll) {
-            finishedScrolling();
-            initialAutoScroll = false;
-          }
-          else {
-            console.log('auto scrolling to `' + state + '`');
-            $document.scrollToElementAnimated(stateElement).then(finishedScrolling);
-          }
+          $document.scrollToElementAnimated(stateElement).then(finishedScrolling);
         }
       };
 
       scope.$watch('activeState', autoScrolling);
 
       $document.bind('scroll', manualScrolling);
-
-      manualScrolling();
     };
 
     return {
