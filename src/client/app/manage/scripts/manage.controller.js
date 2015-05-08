268b9546bf3a117665f712fa849aa6f486ac9b3b
Tom Ladendorf <tom@tomladendorf.com>
2015-05-08 15:11:57 -0400

    Fixed manage page for demo

--- src/client/app/manage/scripts/manage.controller.js
+++ src/client/app/manage/scripts/manage.controller.js
@@ -13,6 +13,7 @@
     vm.workRequests = [];
     vm.newProject = null;
     vm.formatWorkRequests = null;
+    vm.go = null;
 
     vm.activate = function() {
       logger.info('Activated Work Request Single View');
@@ -54,6 +55,14 @@
 
     vm.newProject = function() {
       $state.go('submit-work.flow');
+    };
+
+    vm.go = function(project) {
+      if (project.status == 'Submitted') {
+        $state.go('timeline');
+      } else {
+        $state.go('submit-work.flow');
+      }
     };
 
     vm.activate();
