8141917d60aa29f39edc49f16b284ea77c330124
samsep <ssepasi@appirio.com>
2015-05-31 20:57:37 -0700

    add specs

--- src/client/app/layout/scripts/homeLink.directive.js
+++ src/client/app/layout/scripts/homeLink.directive.js
@@ -1,25 +1,25 @@
-// (function () {
-// 'use strict';
+(function () {
+'use strict';
 
-// angular.module('app.layout')
-// .directive('ngHomeLink', homeLinkDirective);
+angular.module('app.layout')
+.directive('ngHomeLink', homeLinkDirective);
 
-// homeLinkDirective.$inject = [];
+homeLinkDirective.$inject = [];
 
-// function homeLinkDirective() {
-//   var directive = {
-//     restrict: 'A',
-//     controller: HomeLinkDirectiveController
-//   };
+function homeLinkDirective() {
+  var directive = {
+    restrict: 'A',
+    controller: HomeLinkDirectiveController
+  };
 
-//   return directive;
-// }
+  return directive;
+}
 
-// HomeLinkDirectiveController.$inject = ['AuthService', '$scope'];
+HomeLinkDirectiveController.$inject = ['AuthService', '$scope'];
 
-// function HomeLinkDirectiveController(AuthService, $scope) {
-//    $scope.getPath = function() {
-//     return AuthService.isAuthenticated() ? '/#/manage' : '#/'
-//   }
-// }
-// })();
+function HomeLinkDirectiveController(AuthService, $scope) {
+   $scope.getPath = function() {
+    return AuthService.isAuthenticated() ? '/#/manage' : '#/'
+  }
+}
+})();
