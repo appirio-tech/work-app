bb9dcf6d6b71a5c472683e1a5b1f20672af85ffb
Alex Tran <vietnogi@gmail.com>
2015-06-26 17:48:37 -0700

    fix login and add specs back in;

--- src/client/app/layout/specs/homeLink.directive.spec.js
+++ src/client/app/layout/specs/homeLink.directive.spec.js
@@ -1,44 +1,43 @@
-// test is too brittle
-// 'use strict';
-// describe('HomeLinkDirective', function() {
-//   var $compile,
-//   $rootScope,
-//   AuthService,
-//   element;
+'use strict';
+describe('HomeLinkDirective', function() {
+  var $compile,
+  $rootScope,
+  AuthService,
+  element;
 
-//   beforeEach(inject(function(_$compile_, _$rootScope_, _AuthService_){
-//     $compile = _$compile_;
-//     $rootScope = _$rootScope_;
-//     AuthService = _AuthService_;
-//     element = $compile("<a ng-home-link=true href={{getPath()}}></a>")($rootScope);
-//   }));
+  beforeEach(inject(function(_$compile_, _$rootScope_, _AuthService_){
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+    AuthService = _AuthService_;
+    element = $compile("<a ng-home-link=true href={{getPath()}}></a>")($rootScope);
+  }));
 
 
-//  context('when the user is authenticated', function() {
-//   beforeEach(function() {
-//     // TODO: need to stub and restore
-//     AuthService.isAuthenticated = function() {
-//       return true;
-//     }
-//     $rootScope.$digest();
-//   });
+ context('when the user is authenticated', function() {
+  beforeEach(function() {
+    // TODO: need to stub and restore
+    AuthService.isAuthenticated = function() {
+      return true;
+    }
+    $rootScope.$digest();
+  });
 
-//   it('should set the ASP path to manage page "#/manage"', function() {
-//     expect(element.attr('href')).to.equal("#/manage")
-//   });
-//  });
+  it('should set the ASP path to manage page "#/manage"', function() {
+    expect(element.attr('href')).to.equal("#/manage")
+  });
+ });
 
-//  context('when the user is not authenticated', function() {
-//   beforeEach(function() {
-//     // TODO: need to stub and restore
-//     AuthService.isAuthenticated = function() {
-//       return false;
-//     }
-//     $rootScope.$digest();
-//   });
+ context('when the user is not authenticated', function() {
+  beforeEach(function() {
+    // TODO: need to stub and restore
+    AuthService.isAuthenticated = function() {
+      return false;
+    }
+    $rootScope.$digest();
+  });
 
-//   it('should set the ASP path to landing page "#/"', function() {
-//     expect(element.attr('href')).to.equal("#/")
-//   });
-// });
-// });
+  it('should set the ASP path to landing page "#/"', function() {
+    expect(element.attr('href')).to.equal("#/")
+  });
+});
+});
