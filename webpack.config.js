f1410b8ac62ef6bdc1e427f5397180a3c22f7a2b
Andrew Selbie <aselbie@gmail.com>
2015-12-30 14:52:53 -0800

    Check for TRAVIS_BRANCH to set ENV variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 require('./node_modules/coffee-script/register');
 
+if (['master', 'dev', 'qa'].indexOf(process.env.TRAVIS_BRANCH) > -1) {
+  process.env.ENV = process.env.TRAVIS_BRANCH;
+}
+
 config = require('appirio-tech-webpack-config')({
   dirname: __dirname,
   entry: {
