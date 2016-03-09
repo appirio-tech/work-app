d84f11afac7a5c23d7be8ca57fa98cde8cd512ca
Andrew Selbie <aselbie@gmail.com>
2016-03-09 12:13:37 -0800

    Test framework updates

--- tests/tests.webpack.js
+++
@@ -1,9 +0,0 @@
-// This file is an entry point for angular tests
-// Avoids some weird issues when using webpack + angular.
-
-require('angular');
-require('angular-mocks/angular-mocks');
-require('sinon');
-
-var testsContext = require.context(".", true, /.spec$/);
-testsContext.keys().forEach(testsContext);
