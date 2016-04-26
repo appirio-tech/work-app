0d9fb8c25c301b9bbb6c0e2ff248925423ecbfbc
Andrew Selbie <aselbie@gmail.com>
2016-04-26 16:47:30 -0700

    Still need to set ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
 require('./node_modules/coffee-script/register')
 
+if (process.env.TRAVIS_BRANCH === 'master') {
+  process.env.ENV = 'PROD'
+}	else if (process.env.TRAVIS_BRANCH === 'qa') {
+  process.env.ENV = 'QA'
+}	else {
+  process.env.ENV = 'DEV'
+}
+
 const config = require('appirio-tech-webpack-config')({
   dirname: __dirname,
   entry: {
